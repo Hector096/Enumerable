7e37ee1427a73ae7f12d8530465989e2a470b256
Whoistolu <digittolu25@gmail.com>
2021-12-10 16:15:53 +0100

    Fix more linter errors

--- myList.rb
+++ myList.rb
@@ -14,20 +14,20 @@ def each(&data)
 
 # Create our list
   list = MyList.new(1, 2, 3, 4)
-=> #<MyList: @list=[1, 2, 3, 4]>
+# => #<MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
   list.all? {|e| e < 5}
-=> true
+# => true
   list.all? {|e| e > 5}
-=> false
+# => false
 
 # Test #any?
   list.any? {|e| e == 2}
-=> true
+# => true
   list.any? {|e| e == 5}
-=> false
+# => false
 
 # Test #filter
   list.filter {|e| e.even?}
-=> [2, 4]
+# => [2, 4]
