7a7840666ebf216829b4c46e92bf9bcc494791c8
Whoistolu <digittolu25@gmail.com>
2021-12-10 16:12:34 +0100

    Fix linter errors

--- module.rb
+++ module.rb
@@ -1,25 +1,23 @@
-module MyEnumerable 
-
+module MyEnumerable
   def all?
-
-      each do |e|
-          return false unless yield(e)
-      end
-      true
+    each do |e|
+      return false unless yield(e)
+    end
+    true
   end
 
   def any?
-      each do |e|
-          return false if yield(e)
-      end
-      true
+    each do |e|
+      return false if yield(e)
+    end
+    true
   end
 
-  def filter? 
-      result = []
-      each do |e|
-          result << e if yield(e)
-      end
-      result
+  def filter?
+    result = []
+    each do |e|
+      result << e if yield(e)
+    end
+    result
   end
 end
